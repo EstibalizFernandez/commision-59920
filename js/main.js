48349ff898bb180171c0ef867aa1a0e3cac43411
Estibaliz Fernandez <estibalizfpalacios@gmail.com>
2024-10-19 21:24:36 +0200

    formularios con estilo

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/*
+
+// valores
+const proyecto ="marketing nómanda";
+const creacion = "2024";
+const presupuesto = "1582.23";
+// console.log
+console.log(proyecto);
+// siempre devuelven strings
+ /*const miPropt=prompt("nombre del proyecto");
+console.log(miPropt);
+// alert
+alert(miPropt);
+console.log(alert);
+// pasar a entero
+const creacionNumer = parseInt(creacion);
+console.log(creacionNumer)
+// pasar a decimal
+const decimales = parseFloat(presupuesto);
+console.log(decimales);
+
+*/
