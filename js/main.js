9aed4fcbb79e5c81a28f36b82399cf8c33b987b3
Estibaliz Fernandez <estibalizfpalacios@gmail.com>
2024-11-12 20:10:05 +0100

    añadido popper via CDN

--- js/main.js
+++ js/main.js
@@ -20,29 +20,3 @@ const decimales = parseFloat(presupuesto);
 console.log(decimales);
 
 */
-
-document.addEventListener("DOMContentLoaded", function () {
-  // Selecciona todas las filas de tarjetas
-  const rows = document.querySelectorAll(".cards-row");
-
-  // Agrega el evento click a cada opción del dropdown
-  document.querySelectorAll(".dropdown-item").forEach((item) => {
-    item.addEventListener("click", function (e) {
-      e.preventDefault(); // Previene el comportamiento por defecto del enlace
-
-      // Obtiene el valor de la opción seleccionada
-      const selectedValue = this.getAttribute("data-value");
-
-      // Oculta todas las filas de tarjetas
-      rows.forEach((row) => {
-        row.style.display = "none";
-      });
-
-      // Muestra la fila de tarjetas correspondiente a la selección
-      const selectedRow = document.getElementById(selectedValue);
-      if (selectedRow) {
-        selectedRow.style.display = "flex";
-      }
-    });
-  });
-});
